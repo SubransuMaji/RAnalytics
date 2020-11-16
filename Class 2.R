19c070c6e052230d69bfa084e6bec331e9c27e93
SubransuMaji <subransu.maji@gmail.com>
2020-11-16 23:49:47 +0530

    cc

--- Class 2.R
+++ Class 2.R
@@ -1,5 +1,5 @@
 #Vecotr----
-x = c(1,2,3,6) #control + enter or Run button
+x = c(2,4,6,8) #control + enter or Run button
 #vector of single data type
 x2 = c('a','b', 'Dhiraj')
 x2
@@ -8,5 +8,6 @@ class(x2)
 x
 mean(x)
 sum(x)
-
+x3= 1:100
+Class x3
 #Matrix----
